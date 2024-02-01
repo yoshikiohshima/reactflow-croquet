52ed97012a7d0f68ef9b120eda9cf48c37d25a03
Yoshiki Ohshima <yoshiki.ohshima@acm.org>
2024-02-01 14:56:21 -0800

    use croquet-react 1.4.1, and use CroquetRoot

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import {CroquetRoot} from '@croquet/react';
 
 import {FlowModel} from "./model.ts";
 import FlowView from './view';
@@ -11,17 +12,25 @@ import {
     App as CroquetApp
 } from "@croquet/react";
 
+function App() {
+  const sessionParams = {
+    name: import.meta.env["VITE_CROQUET_APP_NAME"] || CroquetApp.autoSession("q"),
+    apiKey: import.meta.env["VITE_CROQUET_API_KEY"],
+    tps: 0.5,
+    appId: import.meta.env["VITE_CROQUET_APP_ID"] || "com.exawizards.flow",
+    password: import.meta.env["VITE_CROQUET_PASSWORD"] || CroquetApp.autoPassword(),
+    model: FlowModel,
+    eventRateLimit: import.meta.env.EVENT_RATE_LIMIT || 60,
+  };
+  return (
+    <CroquetRoot sessionParams={sessionParams}>
+      <FlowView/>
+    </CroquetRoot>
+  )
+}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <React.StrictMode>
-      <InCroquetSession
-        name={import.meta.env["VITE_CROQUET_APP_NAME"] || CroquetApp.autoSession("q")}
-        apiKey={import.meta.env["VITE_CROQUET_API_KEY"]}
-        tps={0.5}
-        appId={import.meta.env["VITE_CROQUET_APP_ID"] || "com.exawizards.flow"}
-        password={import.meta.env["VITE_CROQUET_PASSWORD"] || CroquetApp.autoPassword()}
-        model={FlowModel}
-        eventRateLimit={import.meta.env["EVENT_RATE_LIMIT"] || 60}>
-         <FlowView />
-      </InCroquetSession>
+  <React.StrictMode>
+      <App/>
   </React.StrictMode>,
 )
